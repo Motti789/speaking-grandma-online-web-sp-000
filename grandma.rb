5f82eba062d19179aaa1b6ecac6f7d5e4e84dffb
Motti Wilschanski <mchillywilly@gmail.com>
2021-06-25 17:16:52 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,7 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma("phrase")
   phrase.upcase
-  if "I LOVE YOU GRANDMA!"
+  if "I LOVE YOU GRANDMA!" == .upcase
     puts "I LOVE YOU TOO PUMPKIN!"
   elsif "Hi Nana, how are you?"
     puts "HUH?! SPEAK UP, SONNY!"
